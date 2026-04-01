b6f659fd0668e707a0abf82b83cd31d836a5b64b
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-04-01 10:10:51 +0900

    feat: style preview

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,14 +1,12 @@
-import "../styles/Section.css";
-
 export default function Section({ title, categoryItems }) {
   return (
     <section>
       <h3>{title}</h3>
       {categoryItems.map((item) => {
         return (
-          <div key={item.id}>
-            <span>{item.label}: </span>
-            <span>{item.value}</span>
+          <div className="item-view" key={item.id}>
+            <span className="label">{item.label}: </span>
+            <span className="value">{item.value}</span>
           </div>
         );
       })}
